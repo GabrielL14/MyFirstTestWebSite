cc7c4d5798f375f0e4d9b6797854cd3111023b9e
GabrielL14 <44418047+GabrielL14@users.noreply.github.com>
2023-08-31 07:42:45 -0400

    Update fshg.js

--- fshg.js
+++ fshg.js
@@ -1,6 +1,8 @@
 Main();
 
 function Main(){
+    document.open();
+    document.write("");
     document.close();
-    document.body.innerHTML = "<p>kjhgfdfgh</p>";
+    document.body.innerHTML = "<h1>PAPIPAPOIN</h1><p>kjhgfdfgh</p>";
 }
