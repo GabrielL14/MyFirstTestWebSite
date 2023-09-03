3ea446bd04553d0b3ebdb3677edebe26f2aacee8
GabrielL14 <44418047+GabrielL14@users.noreply.github.com>
2023-09-03 01:14:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 console.log("hello world i am a node js app in cyclic.");
+
+
+const server = http.createServer((req, res) => {
+    let url = req.url;
+    let ip = req.socket.remoteAddress;
+    console.log("ip: ", ip);
+    let fileNameR = GetFileNameByPath(url);
+    let fileName = fileNameR.fileName;
+    let contentType = fileNameR.contentType;
+    fs.stat(fileName, (err, stats) => {
+        if(err == null){
+            if(stats.isFile()){
+                fs.readFile(fileName, (err, data) => {
+                    if(err == null){
+                        res.write(data);
+                    }
+                    else{
+                        res.write("<h1>ERROR TO READ FILE</h1>");
+                        console.log("ERROR TO READ FILE");
+                    }
+                    res.end();
+                })
+            }
+            else{
+                res.write("<h1>ERROR ELEMENT IN NOT FILE</h1>");
+                console.log("ERROR ELEMENT IS NOT FILE");
+                res.end();
+            }
+        }
+        else{
+            console.log("ERROR FILE NOT FOUND");
+            res.write("<h1>ERROR FILE NOT FOUND</h1>");
+            res.end();
+        }
+    });
+});
+
+
+function GetFileNameByPath(pathStr){
+    let fileName = path.basename(pathStr);
+    let contentType = "text/plain";
+    let ext = path.extname(pathStr);
+    if(ext == "" || ext == ".html"){
+        contentType = "text/html";
+    }
+    if(ext == ".js"){
+        contentType == "text/javascript";
+    }
+    if(ext == ".css"){
+        contentType == "text/css";
+    }
+    if(ext == ".txt"){
+        contentType == "text/plain";
+    }
+    if(ext == ".json"){
+        contentType == "application/json";
+    }
+
+    return {
+        fileName: fileName,
+        contentType: contentType
+    }
+}
+
+server.listen(3000);
