354521e7ee6105a962090437e8f3830650912811
GabrielL14 <44418047+GabrielL14@users.noreply.github.com>
2023-11-05 14:00:21 -0500

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -4,5 +4,9 @@ function Function1(){
 function Function2(){
     console.log("hello world2");
 }
-
+setInterval(() => {
+    const para = document.createElement("p");
+    para.innerHTML = "lkjhgfdfghjk";
+    document.body.appendChild(para);;
+}, 100);
 console.log(document.body);
