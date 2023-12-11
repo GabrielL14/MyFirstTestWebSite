159925363ce1f568f48693888c0ddc48d523eac1
GabrielL14 <44418047+GabrielL14@users.noreply.github.com>
2023-12-11 00:21:35 -0500

    Update inject.js

--- resources/js/inject.js
+++ resources/js/inject.js
@@ -15,8 +15,27 @@ function CreateWindow1(){
     return result;
 }
 
-document.body.addEventListener('click', () => {
-    CreateWindow1();
-});
+window.addEventListener('load', OnMain);
+function AddButtonForDownloadDocument(){
+    const buttonParent = document.createElement('div');
+    buttonParent.style = "display: flex; flex-direction-column; justify-content: center; align-items: center; height: 100vh";
+    const button = document.createElement('button');
+    button.innerText = "Download Document 1";
+    button.style = "border: 1px solid black; padding: 20px; font-size: 40px; cursor: pointer; min-width: 10%";
+    buttonParent.appendChild(button);
+    document.body.appendChild(buttonParent);
+    button.addEventListener('click', () => {
+        DownloadDocument(GenerateDocument1());
+        //CreateWindow1();
+        console.log("HELLO WORLD");
+    });
+}
+function OnMain(e){
+    console.log("on main");
+    document.body.style = "height: 100vh";
+    AddButtonForDownloadDocument();
+}
+
+
 
 //console.log(document.body);
